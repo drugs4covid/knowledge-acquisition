c97506c7ba376f9192c23a918bba29c53e2656d8
oscarpiette <89985554+oscarpiette@users.noreply.github.com>
2022-04-05 12:46:00 +0200

    Updated working draft

--- check_cord19_update.py
+++ check_cord19_update.py
@@ -1,5 +1,30 @@
 # 
 
+def cord19(date):
+	"""
+	Function to assess if an update to the CORD-19 was made at the day specified.
+	
+	Parameters:
+	
+	 - date (String): A string indicating the day to check for.
+	 
+	 Returns:
+	 
+	 - False (Boolean): If no update was made to the CORD-19 in the day specified.
+	 - True (Boolean): If an update was detected on the day specified.
+	"""
+	import requests
+	try:
+		r = requests.get("https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/{}/document_parses.tar.gz".format(date), stream=True)
+		if r.status_code == 200:
+			return True
+		
+	except Exception:
+		print("Warning, the request failed", Exception)
+		pass
+	return False
+
+
 def check_cord19_update(number_of_days=7):
 	"""
 	Function to check if an update has been provided for the CORD-19 corpus in the last x days.
@@ -18,17 +43,25 @@ def check_cord19_update(number_of_days=7):
 	today = date.today()
 	range_days = []
 	for day in range(number_of_days):
-		range_days.append(str(today - timedelta(day)))
+		range_days.append(str(today - timedelta(day)))		
 	
+	found_update=False
 	for day in range_days:
 		try:
-			print("here I try to get the document cord-19_{}.tar.gz from the web".format(day))
-			# prueba = wget.download("https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases/cord-19_{}.tar.gz".format(day))
+			# print("here I try to get the document cord-19_{}.tar.gz from the web".format(day))
+			if cord19(day):
+				found_update=True
+				break
 			next
 		except Exception:
-			print("CORD-19 was not updated in the day {}".format(day))
+			print("An error occurred while trying to assess if an update was made", Exception)
 
-	print("No CORD-19 update was detected in the last {} days".format(number_of_days))
+	
+	if found_update:
+		print("An update was found on the CORD-19 at the date {}".format(day))
+		return True
+	
+	print("No update was found on the web in the last {} days".format(number_of_days))
 	return False
 	
 	
